abaf3f042117a8b7656ed5da5febfcd6a8ce0dc2
John <jzlowie@gmail.com>
2021-07-19 22:41:04 -0600

    Added employees to each store

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,19 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store3 = Store.find_by(id: 4)
+@store4 = Store.find_by(id: 5)
+@store5 = Store.find_by(id: 6)
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 20)
+@store2.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 30)
+@store3.employees.create(first_name: "Tony", last_name: "Stark", hourly_rate: 200)
+@store3.employees.create(first_name: "Steve", last_name: "Rogers", hourly_rate: 5)
+@store3.employees.create(first_name: "Natasha", last_name: "Romanoff", hourly_rate: 150)
+@store4.employees.create(first_name: "Bruce", last_name: "Banner", hourly_rate: 200)
+@store4.employees.create(first_name: "Chadwick", last_name: "Boseman", hourly_rate: 150)
+@store5.employees.create(first_name: "Thor", last_name: "Odinson", hourly_rate: 500)
+
